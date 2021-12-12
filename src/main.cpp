6442b918f8474fcee58bcf0aac54cc7e59308960
Martin Luptak <martinxluptak@gmail.com>
2021-12-12 15:32:51 +0100

    Add Eigen via FetchContent in CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    Matrix3f a = Matrix3f::Zero();
+    cout << "Matrix[0][0]:" << a(0, 0) << endl;
     return 0;
 }
