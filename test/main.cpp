92295129f44bd479a5faa1583541b6692d585883
Martin Luptak <martinxluptak@gmail.com>
2021-12-14 15:08:08 +0100

    Switch to Conan dependency manager

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by lupta on 12/14/2021.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
