1ac8f338d2ceff91f9fe6668a01ebf19e7441197
Martin Luptak <martinxluptak@gmail.com>
2021-12-21 19:59:13 +0100

    Simplify OCV install script to only use Ninja

--- lib/installOCV.sh
+++ lib/installOCV.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -e
 myRepo=$(pwd)
-
-# create a yes/no prompt
-while true; do
-    read -p "Do you wish to use the Visual Studio compiler for installation?" yn
-    case $yn in
-        [Yy]* ) CMAKE_GENERATOR_OPTIONS=(-G"Visual Studio 16 2019" -A x64); break;; # CMake 3.14+ is required
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+CMAKE_GENERATOR_OPTIONS=(-G"Ninja")
 
 if [  ! -d "$myRepo/opencv"  ]; then
     echo "cloning opencv"
@@ -27,16 +18,12 @@ else
     git pull --rebase
     cd ..
 fi
+
 RepoSource=opencv
 mkdir -p build_opencv
 pushd build_opencv
 CMAKE_OPTIONS=(-DOPENCV_ENABLE_NONFREE:BOOL=ON -DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON)
 set -x
 cmake "${CMAKE_GENERATOR_OPTIONS[@]}" "${CMAKE_OPTIONS[@]}" -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/opencv_contrib/modules -DCMAKE_INSTALL_PREFIX="$myRepo/install/$RepoSource" "$myRepo/$RepoSource"
-echo "************************* $Source_DIR -->debug"
-cmake --build .  --config debug
-echo "************************* $Source_DIR -->release"
-cmake --build .  --config release
-cmake --build .  --target install --config release
-cmake --build .  --target install --config debug
+ninja
 popd
