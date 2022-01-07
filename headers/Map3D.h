1ab7f555bf79472c70bd8b4e9bb661af460ddfd6
witold-p <witold.pacholarz@gmail.com>
2022-01-07 20:57:57 +0100

    Create Map3D files

---
+++ headers/Map3D.h
@@ -0,0 +1,11 @@
+//
+// Created by witek on 07.01.22.
+//
+
+#ifndef BUNDLE_ADJUSTMENT_MAP3D_H
+#define BUNDLE_ADJUSTMENT_MAP3D_H
+
+// test
+int sum(int& a, int&b);
+
+#endif //BUNDLE_ADJUSTMENT_MAP3D_H
---
+++ src/Map3D.cpp
@@ -0,0 +1,10 @@
+//
+// Created by witek on 07.01.22.
+//
+
+#include "Map3D.h"
+
+// test
+int sum(int&a, int& b){
+    return a+b;
+}
