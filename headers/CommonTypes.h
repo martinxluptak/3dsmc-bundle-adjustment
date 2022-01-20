38908c2bc9b4f80e8ec74fc1f5b97fb1c9eb239a
Moustafa Elsharkawy <mostafa1075@gmail.com>
2022-01-20 19:46:15 +0100

    Edit map structure

--- headers/CommonTypes.h
+++ headers/CommonTypes.h
@@ -10,6 +10,8 @@
 using namespace std;
 using namespace cv;
 
+typedef int TrackId;
+
 struct KeyFrame {
   int frame_id;
   Sophus::SE3d pose; // relative to frame 1
@@ -20,10 +22,9 @@ struct KeyFrame {
 
   // Maps the locals 3d points to their corresponding global 3d point in the Map
   // if it exists
-  unordered_map<
-      Vector3d, Vector3d,
-      Eigen::aligned_allocator<pair<Eigen::Vector3d, Eigen::Vector3d>>>
-      points3d_map;
+  // Note: This data is redundant with the map, but makes it faster to index
+  // and implement
+  unordered_map<int, TrackId> global_points_map;
 
   // relative to the current frame
   //  vector<Vector3d> points3d_global; // relative to frame 1
@@ -33,9 +34,11 @@ typedef pair<int, Point2d> Observation;
 
 typedef vector<Observation> Observations;
 
-typedef unordered_map<
-    Vector3d, Observations,
-    Eigen::aligned_allocator<pair<Eigen::Vector3d, Observations>>>
-    Map3D;
+struct Track {
+  Observations observations;
+  Vector3d point;
+};
+
+typedef unordered_map<TrackId, Track> Map3D;
 
 #endif // COMMON_TYPES_H
