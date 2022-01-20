d6027b8265da76bbad42eb5709f485041b3410fe
witold-p <witold.pacholarz@gmail.com>
2022-01-20 13:07:52 +0100

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,7 @@ int main() {
                               255.3, 0.0, 0.0,   1.0};
   Mat intrinsics(3, 3, CV_64FC1, intrinsics_data);
 
-  string filename = string(
-      "../../Data/rgbd_dataset_freiburg1_xyz/"); // SET TO
+  main: string filename = string(".../rgbd_dataset_freiburg1_xyz/");  // SET TO
                                                  // <your_path>/rgbd_dataset_freiburg1_xyz/
 
   // load video
@@ -45,7 +44,6 @@ int main() {
 
     // Transfromation from frame 2 to 1
     Sophus::SE3d T_1_2;
-    vector<Vector3d> points3d_before, point3d_after;
 
     KeyFrame current_frame;
     current_frame.frame_id = sensor.GetCurrentFrameCnt();
@@ -81,7 +79,7 @@ int main() {
 
     current_frame.pose =
         T_1_2.inverse() * current_pose; // global pose for the current frame
-    current_pose = current_frame.pose;  // global pose for the next frame
+    current_pose = current_frame.pose;
 
     //    current_frame.points3d_global = getGlobalPoints3D(current_frame);
     keyframes.push_back(current_frame);
