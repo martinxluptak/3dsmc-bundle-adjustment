4d1d1add2233b4b6fb6495b8edc9592c70761520
witold-p <witold.pacholarz@gmail.com>
2022-01-21 19:10:13 +0100

    Implement SIFT and SURF

--- headers/Detection.h
+++ headers/Detection.h
@@ -6,10 +6,18 @@
 #define BUNDLE_ADJUSTMENT_DETECTION_H
 
 #include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d/nonfree.hpp"
 using namespace std;
-using namespace cv;
+using namespace cv; // SIFT and ORB
+using namespace cv::xfeatures2d; // SURF
 
 void getORB(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
             int &num_features);
+
+void getSIFT(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+             int &num_features);
+
+void getSURF(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+             int &hessian_threshold); // default hessian_threshold = 100
 
 #endif // BUNDLE_ADJUSTMENT_DETECTION_H
--- src/Detection.cpp
+++ src/Detection.cpp
@@ -6,6 +6,18 @@
 
 void getORB(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
             int &num_features) {
-  Ptr<ORB> detector = ORB::create(num_features);
-  detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
+    Ptr<ORB> detector = ORB::create(num_features);
+    detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
+}
+
+void getSIFT(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+            int &num_features) {
+    Ptr<SIFT> detector = SIFT::create(num_features);
+    detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
+}
+
+void getSURF(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+             int &hessian_threshold) {
+    Ptr<SURF> detector = SURF::create(hessian_threshold);
+    detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
 }
