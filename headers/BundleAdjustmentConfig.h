a767b9ae7dce0dbe5577ce20212288d55893cef8
leom97 <leo--msn@hotmail.com>
2022-01-26 23:53:28 +0100

    Reformatting, changed position of include guards, add ceresGlobalProblem and windowedOptimization

--- headers/BundleAdjustmentConfig.h
+++ headers/BundleAdjustmentConfig.h
@@ -2,11 +2,22 @@
 // Created by lupta on 1/26/2022.
 //
 
+#ifndef BUNDLE_ADJUSTMENT_OPTIMIZATIONCONFIG_H
+#define BUNDLE_ADJUSTMENT_OPTIMIZATIONCONFIG_H
+
+#include "OptimizationUtils.h"
+#include <string>
+#include "Eigen/Dense"
+#include <fstream>
+#include <utility>
+#include <ceres/ceres.h>
+#include "sophus/local_parameterization_se3.hpp"
 #include <iostream>
 #include <string>
 
-#ifndef BUNDLE_ADJUSTMENT_OPTIMIZATIONCONFIG_H
-#define BUNDLE_ADJUSTMENT_OPTIMIZATIONCONFIG_H
+using namespace std;
+using namespace Eigen;
+
 
 class BundleAdjustmentConfig {
 
@@ -24,14 +35,30 @@ public:
     int NUM_FEATURES = 1000;
     const uint KEYFRAME_INCREMENT = 10;
 
-    /*
-     *  Define parameters for optimization.
-     */
-    const double HUB_P_REPR = 1e-2; // Huber loss parameter for reprojection constraints
-    const double HUB_P_UNPR = 1e-2; // Huber loss parameter for depth prior (i.e. unprojection constraints)
-    const double WEIGHT_UNPR = 5; // weight for unprojection constraint
-    const double WEIGHT_INTRINSICS = 1e-4;
-    static constexpr double NEG_INF = std::numeric_limits<double>::lowest();   // -inf
+};
+
+class ceresGlobalProblem{
+public:
+    ceresGlobalProblem(const double hubPRepr, const double weightIntrinsics, const double weightUnpr,
+                       const double hubPUnpr, const ceres::Solver::Options &options,
+                       ceres::LossFunctionWrapper *lossFunctionRepr, ceres::LossFunctionWrapper *lossFunctionUnpr,
+                       ceres::LocalParameterization *localParametrizationSe3)
+            : HUB_P_REPR(hubPRepr), WEIGHT_INTRINSICS(weightIntrinsics), WEIGHT_UNPR(weightUnpr), HUB_P_UNPR(hubPUnpr),
+              options(options), loss_function_repr(lossFunctionRepr), loss_function_unpr(lossFunctionUnpr),
+              local_parametrization_se3(localParametrizationSe3) {}
+
+    const double HUB_P_REPR; // Huber loss parameter for reprojection constraints
+    const double WEIGHT_INTRINSICS;
+    const double WEIGHT_UNPR; // weight for unprojection constraint. Relative to the reprojection constraints, who have a weight of 1
+    const double HUB_P_UNPR; // Huber loss parameter for depth prior (i.e. unprojection constraints)
+
+    ceres::Solver::Options options;
+
+    ceres::LossFunctionWrapper *loss_function_repr;
+    ceres::LossFunctionWrapper *loss_function_unpr;
+
+    ceres::LocalParameterization *local_parametrization_se3;
+
 };
 
 #endif //BUNDLE_ADJUSTMENT_OPTIMIZATIONCONFIG_H
