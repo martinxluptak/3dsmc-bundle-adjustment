19d012f5bcc4b8653a062eaea2e214037b61c578
witold-p <witold.pacholarz@gmail.com>
2022-01-27 00:57:33 +0100

    Delete keypoints and descriptors with corresponding negative depth values (commented line in getORB())

--- src/Detection.cpp
+++ src/Detection.cpp
@@ -4,20 +4,46 @@
 
 #include <Detection.h>
 
-void getORB(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+void getORB(const Mat &rgb, const Mat &depth,  vector<KeyPoint> &keypoints, Mat &descriptors,
             int &num_features) {
     Ptr<ORB> detector = ORB::create(num_features);
-    detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
+    detector->detectAndCompute(rgb, noArray(), keypoints, descriptors);
+//    pruneMINF(depth, keypoints, descriptors);
+//    cout << "key: " << keypoints.size() << endl;
+//    cout << "desc: " << descriptors.size() << endl;
 }
 
-void getSIFT(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+void getSIFT(const Mat &rgb, const Mat &depth, vector<KeyPoint> &keypoints, Mat &descriptors,
             int &num_features) {
     Ptr<SIFT> detector = SIFT::create(num_features);
-    detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
+    detector->detectAndCompute(rgb, noArray(), keypoints, descriptors);
+    pruneMINF(depth, keypoints, descriptors);
 }
 
-void getSURF(const Mat &frame1, vector<KeyPoint> &keypoints1, Mat &descriptors1,
+void getSURF(const Mat &rgb, const Mat &depth, vector<KeyPoint> &keypoints, Mat &descriptors,
              int &hessian_threshold) {
     Ptr<SURF> detector = SURF::create(hessian_threshold);
-    detector->detectAndCompute(frame1, noArray(), keypoints1, descriptors1);
+    detector->detectAndCompute(rgb, noArray(), keypoints, descriptors);
+    pruneMINF(depth, keypoints, descriptors);
+}
+
+void pruneMINF(Mat depth, vector<KeyPoint> &keypoints, Mat &descriptors){
+    vector<int> good_ids;
+    vector<KeyPoint> good_keypoints;
+    Mat good_descriptors;
+
+    for (int id=0; id < keypoints.size(); id++){
+        int x = static_cast<int>(keypoints[id].pt.x);
+        int y = static_cast<int>(keypoints[id].pt.y);
+        if (depth.at<int>(y, x) > 0)
+            good_ids.push_back(id);
+    }
+
+    for (int i=0; i<good_ids.size(); i++){
+        good_keypoints.push_back(keypoints[i]);
+        good_descriptors.push_back(descriptors.row(i));
+    }
+
+    keypoints = good_keypoints;
+    descriptors = good_descriptors;
 }
