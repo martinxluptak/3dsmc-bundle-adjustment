79614cb477f40e3f28460aaa32b09a0b8f0cc73f
Moustafa Elsharkawy <mostafa1075@gmail.com>
2022-02-04 21:57:01 +0100

    Adding map and poses plotting using pangolin

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@
 #include "sophus/local_parameterization_se3.hpp"
 #include "OptimizationUtils.h"
 #include "BundleAdjustmentConfig.h"
+#include "helpers.h"
+#include <pangolin/display/display.h>
+#include <pangolin/display/view.h>
+#include <pangolin/scene/axis.h>
+#include <pangolin/scene/scenehandler.h>
 
 using namespace cv;
 using namespace std;
@@ -66,8 +71,24 @@ void tracking_step(VirtualSensor &sensor, vector<KeyFrame> &keyframes, Map3D &ma
 
 }
 
-int main() {
+void draw_poses(const vector<KeyFrame>& keyframes){
+    for(auto& kf: keyframes) {
+        render_camera(kf.T_w_c.matrix(), 3.0f, new u_int8_t[3]{0, 125, 0}, 0.1f);
+    }
+}
 
+ void draw_map(const Map3D& map) {
+    glColor3b(0, 0, 0);
+    glPointSize(3.0);
+    glBegin(GL_POINTS);
+
+    for(auto& point: map){
+        pangolin::glVertex(point.second.point);
+    }
+    glEnd();
+}
+
+int main() {
     // Some configurations
     auto cfg = BundleAdjustmentConfig();
     auto cfg_optimization = ceresGlobalProblem();
@@ -81,9 +102,10 @@ int main() {
     vector<KeyFrame> keyframes;
     Vector4d intrinsics_initial = read_camera_intrinsics_from_file(cfg.CAMERA_DEFAULT_INTRINSICS_PATH);
     auto intrinsics_optimized(intrinsics_initial);  // to be later optimized
-
+    bool is_tracking_finished = false;
+    bool is_optimization_finished = true;
     // Uncomment this and comment from that onwards, for debugging
-//    optimizeDebug(cfg_optimization, intrinsics_initial, intrinsics_optimized, 0);
+    // optimizeDebug(cfg_optimization, intrinsics_initial, intrinsics_optimized, 0);
 
     // Temporary variables
     LandmarkId landmark_id = 0;
@@ -99,26 +121,57 @@ int main() {
     // Process every frame in the sequence, optimize once in a while
     bool do_windowed_optimization = (cfg_optimization.window_size > 0), do_global_optimization = (
             cfg_optimization.window_size < 0);
-    while (sensor.ProcessNextFrame()) {
-        tracking_step(sensor, keyframes, map, cfg, landmark_id, intrinsics_initial);    // Tracking step
-        if (do_windowed_optimization && keyframes.size() % cfg_optimization.frame_frequency == 0 &&
-            keyframes.size() >= cfg_optimization.window_size) {
-            windowOptimize(cfg_optimization, keyframes.size() - cfg_optimization.window_size, keyframes.size() - 1,
-                           keyframes, map, intrinsics_initial, intrinsics_optimized);
-        }   // Optimization step
-    }
-    if (do_windowed_optimization && keyframes.size() % cfg_optimization.frame_frequency != 0) {
-        windowOptimize(cfg_optimization,
-                       keyframes.size() - cfg_optimization.window_size,
-                       keyframes.size() - 1, keyframes, map, intrinsics_initial, intrinsics_optimized);
-    }   // Leftovers
 
-    // Run instead global optimization
-    if (do_global_optimization) {
-        windowOptimize(cfg_optimization, 0, keyframes.size() - 1, keyframes, map, intrinsics_initial,
-                       intrinsics_optimized);
-    }
+    // Adapted from Pangolin example: https://github.com/stevenlovegrove/Pangolin/blob/master/examples/SimpleScene/main.cpp
+    pangolin::CreateWindowAndBind("Main",640,480);
+    glEnable(GL_DEPTH_TEST);
 
+    // Define Projection and initial ModelView matrix
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
+        pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisNegY)
+    );
+
+    pangolin::View& d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
+            .SetHandler(new pangolin::Handler3D(s_cam));
+    glClearColor(0.95f, 0.95f, 0.95f, 1.0f);  // background
+
+    while( !pangolin::ShouldQuit() )
+    {
+        // Clear screen and activate view to render into
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Swap frames and Process Events
+        d_cam.Activate(s_cam);
+        draw_poses(keyframes);
+        draw_map(map);
+        pangolin::FinishFrame();
+
+        is_tracking_finished = !sensor.ProcessNextFrame();
+        if (!is_tracking_finished) {
+            tracking_step(sensor, keyframes, map, cfg, landmark_id, intrinsics_initial);    // Tracking step
+            if (!is_optimization_finished && do_windowed_optimization && keyframes.size() % cfg_optimization.frame_frequency == 0 &&
+                keyframes.size() >= cfg_optimization.window_size) {
+                windowOptimize(cfg_optimization, keyframes.size() - cfg_optimization.window_size, keyframes.size() - 1,
+                               keyframes, map, intrinsics_initial, intrinsics_optimized);
+            }   // Optimization step
+        }
+        if (is_tracking_finished && !is_optimization_finished && do_windowed_optimization && keyframes.size() % cfg_optimization.frame_frequency != 0) {
+            windowOptimize(cfg_optimization,
+                           keyframes.size() - cfg_optimization.window_size,
+                           keyframes.size() - 1, keyframes, map, intrinsics_initial, intrinsics_optimized);
+            is_optimization_finished = true;
+        }   // Leftovers
+
+        // Run instead global optimization
+        if (is_tracking_finished && !is_optimization_finished && do_global_optimization) {
+            windowOptimize(cfg_optimization, 0, keyframes.size() - 1, keyframes, map, intrinsics_initial,
+                           intrinsics_optimized);
+            is_optimization_finished = true;
+        }
+
+
+    }
     cout << endl << "End of optimization. Writing output to file..." << endl;
 
     // Adjust the output so that the first pose aligns with ground_truth.txt, and is not just identity
@@ -128,7 +181,6 @@ int main() {
 
     // Write output to file
     write_keyframe_poses_to_file(cfg.OUTPUT_POSES_PATH, keyframes);
-
 
     return 0;
 }
