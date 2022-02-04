af2c16c6553b9b9780cfebd76a73a26e198af8cc
leom97 <leo--msn@hotmail.com>
2022-02-04 19:02:55 +0100

    Best settings for freiburg1_xyz

--- headers/BundleAdjustmentConfig.h
+++ headers/BundleAdjustmentConfig.h
@@ -28,16 +28,16 @@ public:
     const string CAMERA_DEFAULT_INTRINSICS_PATH = "../../Data/ros_default_intrinsics.txt";
     const string DATASET_FILEPATH = "../../Data/rgbd_dataset_freiburg1_xyz/"; // SET TO <your_path>/rgbd_dataset_freiburg1_xyz/
     const string GROUND_TRUTH_PATH = "../../Data/rgbd_dataset_freiburg1_xyz/groundtruth.txt";
-    const string OUTPUT_POSES_PATH = "../../output/freiburg1_xyz_poses.txt"; // output
+    const string OUTPUT_POSES_PATH = "../../output/freiburg1_xyz_poses.txt"; // output:
 
     /*
      *  Keypoint extraction + feature matching
      */
-    const uint KEYFRAME_INCREMENT = 10;
-    const string DETECTOR = "SURF"; // options: ORB, SIFT, SURF
+    const string DETECTOR = "SIFT"; // options: ORB, SIFT, SURF
     int NUM_FEATURES = 1000; // hyperparameter for ORB and SIFT
-    int HESSIAN_THRES = 100; // hyperparameter for SURF; default value: 100
-    const float LOWE_THRESHOLD = 0.7;
+    int HESSIAN_THRES = 8000; // hyperparameter for SURF; default value: 100
+    const uint KEYFRAME_INCREMENT = 5;
+    const float LOWE_THRESHOLD = .7;
 
 };
 
@@ -46,11 +46,11 @@ public:
 
     const double HUB_P_REPR = 1e-3; // Huber loss parameter for reprojection constraints
     const double WEIGHT_INTRINSICS = 1e-6;
-    const double WEIGHT_UNPR = 2; // weight for unprojection constraint. Relative to the reprojection constraints, who have a weight of 1
+    const double WEIGHT_UNPR = 10; // weight for unprojection constraint. Relative to the reprojection constraints, who have a weight of 1
     const double HUB_P_UNPR = 1e-3; // Huber loss parameter for depth prior (i.e. unprojection constraints)
 
-    const int frame_frequency = 50;  // wait this many frames to do optimization again
-    const int window_size = 50; // how many keyframes are we optimizing for every window? Put -1 to have a unique window, put 0 to skip optimization
+    const int frame_frequency = 10;  // wait this many frames to do optimization again
+    const int window_size = 30; // how many keyframes are we optimizing for every window? Put -1 to have a unique window, put 0 to skip optimization
 
     ceres::Solver::Options options;
 
